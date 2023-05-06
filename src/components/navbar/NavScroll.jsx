e26b91719ec0c922ac03c1f6cddf7403e83d0f04
Jagdish Kushwaha <pwdacait7@gmail.com>
2023-05-06 14:56:51 +0530

    made navbar responsive

--- src/components/navbar/NavScroll.jsx
+++ src/components/navbar/NavScroll.jsx
@@ -14,51 +14,50 @@ function NavScroll() {
   return (
     <>
       <Navbar bg="light" expand="lg">
-        <Container fluid className="flex-space" style={{display:'flex',justifyContent:'space-between'}}>
+        <Container
+          fluid
+          style={{ display: "flex", justifyContent: "space-between" }}
+          className="nav1 flex-space"
+        >
           <Navbar.Brand href="/">
             <img className="logo" src="assets/logo.png" alt="logo" />
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-          <Navbar.Collapse id="navbarScroll">
-          <Form className="d-flex">
-            <Form.Control
-              type="search"
-              placeholder="Search"
-              className="me-2"
-              aria-label="Search"
-            />
-            <Button variant="outline-success">Search</Button>
-          </Form>
-          <Nav className="me-auto">
-          <div className="social-icons flex-spacee">
-            <FacebookIcon />
-            <TwitterIcon />
-            <LinkedInIcon />
-          </div>
-          </Nav>
+          <Navbar.Collapse id="navbarScroll" className="flex-space nav1-content flex-right" >
+              <Form className="d-flex search-bar">
+                <Form.Control
+                  type="search"
+                  placeholder="Search"
+                  className="me-2"
+                  aria-label="Search"
+                />
+                <Button variant="outline-success">Search</Button>
+              </Form>
+              <div className="social-icons">
+              <Nav.Link href="/"><FacebookIcon /></Nav.Link>
+              <Nav.Link href="/"><TwitterIcon /></Nav.Link>
+              <Nav.Link href="/"><LinkedInIcon /></Nav.Link>
+                
+                
+                
+              </div>
           </Navbar.Collapse>
         </Container>
       </Navbar>
 
       <Navbar bg="dark" variant="dark">
         <Container
-          style={{
-            display: "flex",
-            justifyContent: "end",
-            alignItems: "center",
-          }}
+          
+          className="nav2"
         >
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-          <Nav>
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="#blog">Blog</Nav.Link>
-            <Nav.Link href="#compare">Compare</Nav.Link>
-            <Nav.Link href="#faqs">FAQs</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
-          </Nav>
-            </Navbar.Collapse>
+            <Nav>
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/about">About</Nav.Link>
+              <Nav.Link href="#blog">Blog</Nav.Link>
+              <Nav.Link href="#compare">Compare</Nav.Link>
+              <Nav.Link href="#faqs">FAQs</Nav.Link>
+              <Nav.Link href="#contact">Contact</Nav.Link>
+            </Nav>
         </Container>
       </Navbar>
     </>
--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,25 +1,27 @@
-import React from 'react'
+import React from "react";
 import CardSlider from "../../cardslider/CardSlider";
 import Carouselcomp from "../../carousel/Carouselcomp";
 import Category from "../../category/Category";
-import './Home.css'
+import "./Home.css";
 
 const Home = () => {
   return (
     <>
-      <Carouselcomp/>
-      <div className="main-container">
-        <div className="ad-section-left ad-section"></div>
-        <div className="main-content">
-          <Category />
-          <hr />
-          <CardSlider heading={"Play Schools"} />
-          <CardSlider heading={"Boarding Schools"} />
+      <div className="home-section">
+        <Carouselcomp />
+        <div className="main-container">
+          <div className="ad-section-left ad-section"></div>
+          <div className="main-content">
+            <Category />
+            <hr />
+            <CardSlider heading={"Play Schools"} />
+            <CardSlider heading={"Boarding Schools"} />
+          </div>
+          <div className="ad-section-right ad-section"></div>
         </div>
-        <div className="ad-section-right ad-section"></div>
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
